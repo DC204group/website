0c1d548f5072638d35ed3d7198c09ea5ee724b2c
coryallegory <corymetcalfe@gmail.com>
2026-07-07 19:20:43 -0500

    updates

--- events-data.js
+++ events-data.js
@@ -6,15 +6,23 @@ const events = [
     startDate: "2026-08-06",
     endDate: "2026-08-09",
     location: "LVCC, Las Vegas, NV",
-    description: "The world's largest annual hacking conference.",
+    description: "Annual hacking and security conference held each summer in Las Vegas.",
     link: "https://defcon.org/"
+  },
+  {
+    name: "Prairie Dev Con",
+    startDate: "2026-09-21",
+    endDate: "2026-09-22",
+    location: "Canad Inns Polo Park, Winnipeg, MB",
+    description: "The Largest AI & Software Development Conference on the Prairies!",
+    link: "https://www.prairiedevcon.com/"
   },
   {
     name: "The Long Con",
     startDate: "2026-11-07",
     endDate: "2026-11-08",
-    location: "West End Cultural Centre, 586 Ellice Avenue, Winnipeg, Manitoba",
-    description: "Information security conference by the community for the community.",
+    location: "West End Cultural Centre, Winnipeg, MB",
+    description: "Winnipeg information security conference by the community for the community.",
     link: "https://thelongcon.ca/"
   }
 ];
